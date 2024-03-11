c238b6208b9f0803d06063326a36324556ffa1f8
Arjun E <arjun.e@astriadigital.com>
2024-03-11 18:28:01 +0530

    fix: fix some bug in token section

--- server.js
+++ server.js
@@ -1,14 +1,15 @@
 const express = require('express')
 const cors = require('cors')
-const { getReviews } = require('./googleAuth')
 const { GoogleAuth, OAuth2Client } = require('google-auth-library')
 const { default: axios } = require('axios')
+const bodyParser = require('body-parser')
 const app = express()
 app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
-const clientId = process.env.CLIENT_ID;
-const clientSecret = process.env.CLIENT_SECRET;
-const redirectUri = process.env.REDIRECT_URL;
+const clientId = "379948474597-bgjvqnfhcnd8spll1q99knb9ogo3m7ep.apps.googleusercontent.com"
+const clientSecret = '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';
+const redirectUri = 'http://localhost:4000/oauthcallback';
 
 // Create Oauth2client instance using oauth client id and client_secret
 const client = new OAuth2Client(clientId, clientSecret, redirectUri);
@@ -40,25 +41,27 @@ app.get('/oauth_google', (req, res) => {
         res.redirect(authorizationUrl);
     } catch (error) {
         console.log(error)
-        errorResponse(res, 500, 'internal server error')
+        errorResponse(res, 500, error)
     }
 })
 
 
-app.get('/get-accounts', async (req, res) => {
+app.get('/get_accounts', async (req, res) => {
     const { code } = req.query;
+    console.log({ code })
     try {
         const { tokens } = await client.getToken(code);
-        // client.setCredentials(tokens);
+        client.setCredentials(tokens);
 
         const url = 'https://mybusiness.googleapis.com/v4/mybusiness/accounts'
+        console.log({ token: tokens.access_token })
         const response = await axios.get(url, { headers: { Authorization: `Bearer ${tokens.access_token}` } })
         const accounts = response.data.accounts; // Assuming accounts data is in the response
 
         successResponse(res, accounts)
     } catch (error) {
         console.error(error);
-        errorResponse(res, 500, 'internal server error')
+        errorResponse(res, 500, error)
     }
 })
 
@@ -70,17 +73,18 @@ app.post('/set_account', (req, res) => {
 
         successResponse(res, null)
     } catch (error) {
-        errorResponse(res, 500, 'internal server error')
+        errorResponse(res, 500, error)
     }
 
 })
 
 
 app.get('/get_reviews/:userid', async (req, res) => {
-    const { code } = req.header.Authorization;
+    const { code } = req.query;
     const { userid } = req.params
     try {
         const { tokens } = await client.getToken(code);
+        client.setCredentials(tokens);
 
         // TODO: Write a query to fetch the accountid and locationid using userid
         let accountId = ""
@@ -95,6 +99,11 @@ app.get('/get_reviews/:userid', async (req, res) => {
         successResponse(res, reviews)
 
     } catch (error) {
-        errorResponse(res, 500, 'internal server error')
+        errorResponse(res, 500, error)
     }
+})
+
+
+app.listen(4000, () => {
+    console.log('connected to localhost: 4000')
 })
